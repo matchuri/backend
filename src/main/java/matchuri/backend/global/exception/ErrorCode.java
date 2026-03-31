35bae376dc00dc9630cd34408cfb979cc33b04bd
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-03-31 20:11:32 +0900

    feat: 에러 코드 인터페이스

---
+++ src/main/java/matchuri/backend/global/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package matchuri.backend.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+    String name();
+    HttpStatus getHttpStatus();
+    String getMessage();
+    String format(Object ...args);
+    String getDomainPrefix();
+
+    default String getCode() {
+        return getDomainPrefix() + ((Enum<?>) this).name();
+    }
+}
