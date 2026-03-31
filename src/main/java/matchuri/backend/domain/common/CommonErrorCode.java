0e92eefe0b948c2919b1a3d5cea73d168329341a
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-03-31 21:52:42 +0900

    refactor: CommonErrorCode 에러 추가

--- src/main/java/matchuri/backend/domain/common/CommonErrorCode.java
+++ src/main/java/matchuri/backend/domain/common/CommonErrorCode.java
@@ -14,6 +14,8 @@ public enum CommonErrorCode implements ErrorCode {
     INVALID_PATH_VARIABLE(HttpStatus.BAD_REQUEST, "경로 변수 형식이 올바르지 않습니다. {0}"),
     INVALID_QUERY_PARAMETER(HttpStatus.BAD_REQUEST, "쿼리 파라미터 형식이 올바르지 않습니다. {0}"),
     INVALID_BODY_FIELD(HttpStatus.BAD_REQUEST, "요청 바디 필드가 올바르지 않습니다. {0}"),
+    AUTHENTICATION_REQUIRED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 HTTP 메서드입니다."),
     UNSUPPORTED_MEDIA_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "지원하지 않는 Content-Type 입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
