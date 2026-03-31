cc2b0a0716afecd4740096660a6a0010fb564290
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-04-01 03:22:25 +0900

    chore: 미사용 import 삭제

--- src/test/java/matchuri/backend/api/member/MemberControllerIntegrationTest.java
+++ src/test/java/matchuri/backend/api/member/MemberControllerIntegrationTest.java
@@ -6,7 +6,6 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
