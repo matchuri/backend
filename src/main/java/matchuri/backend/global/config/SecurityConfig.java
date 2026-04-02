bfe2bc8ff07a828e88dc66a424737f33fda4c591
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-04-02 19:54:54 +0900

    fix: cors 설정

--- src/main/java/matchuri/backend/global/config/SecurityConfig.java
+++ src/main/java/matchuri/backend/global/config/SecurityConfig.java
@@ -16,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -55,5 +58,22 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        MatchuriProperties.Cors corsProps = matchuriProperties.getAuth().getCors();
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(corsProps.getAllowedOrigins());
+        configuration.setAllowedMethods(corsProps.getAllowedMethods());
+        configuration.setAllowedHeaders(corsProps.getAllowedHeaders());
+        configuration.setExposedHeaders(corsProps.getExposedHeaders());
+        configuration.setAllowCredentials(corsProps.isAllowCredentials());
+        configuration.setMaxAge(corsProps.getMaxAge());
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
