eab6b93720167d455b0cdb21f3877eb039f9948c
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-04-26 16:53:53 +0900

    refactor: 서비스에 맞게 제목과 내용 설정

--- src/main/java/matchuri/backend/domain/auth/service/EmailVerificationServiceImpl.java
+++ src/main/java/matchuri/backend/domain/auth/service/EmailVerificationServiceImpl.java
@@ -2,6 +2,7 @@ package matchuri.backend.domain.auth.service;
 
 import lombok.RequiredArgsConstructor;
 import matchuri.backend.api.auth.dto.request.EmailVerificationRequest;
+import matchuri.backend.domain.auth.support.vertification.VerificationCodeGenerator;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,12 +14,18 @@ public class EmailVerificationServiceImpl implements EmailVerificationService {
 
     private final JavaMailSender mailSender;
 
+    public static String EMAIL_SUBJECT = "맛추리 회원가입 인증 요청";
+
     @Override
     public void sendTxtEmail(EmailVerificationRequest request) {
+
         SimpleMailMessage smm = new SimpleMailMessage();
+        String code = VerificationCodeGenerator.generateCode();
+
         smm.setTo(request.email());
-        smm.setSubject(request.subject());
-        smm.setText(request.content());
+        smm.setSubject(EMAIL_SUBJECT);
+        smm.setText(code);
+
         try {
             mailSender.send(smm);
             System.out.println("이메일 전송 성공!");
