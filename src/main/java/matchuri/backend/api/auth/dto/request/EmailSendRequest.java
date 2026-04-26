acb141038296c60ee013bf8070ece6e447f0bf8f
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-04-26 17:48:51 +0900

    refactor: request에 불필요한 프로퍼티 제거

--- src/main/java/matchuri/backend/api/auth/dto/request/EmailSendRequest.java
+++ src/main/java/matchuri/backend/api/auth/dto/request/EmailSendRequest.java
@@ -1,8 +1,6 @@
 package matchuri.backend.api.auth.dto.request;
 
 public record EmailSendRequest(
-        String email,
-        String subject,
-        String content
+        String email
 ) {
 }
