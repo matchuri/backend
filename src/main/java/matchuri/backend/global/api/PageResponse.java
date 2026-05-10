c7e4ed52fc0bb4977de6f23d2683a0da0b19c9a8
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-05-11 05:17:19 +0900

    feat: PageResponse 유틸 메서드 추가

--- src/main/java/matchuri/backend/global/api/PageResponse.java
+++ src/main/java/matchuri/backend/global/api/PageResponse.java
@@ -28,4 +28,8 @@ public class PageResponse<T> {
     public static <T> PageResponse<T> mock(List<T> list) {
         return new PageResponse<>(list, PageInfo.mock());
     }
+
+    public static <T> PageResponse<T> ofList(List<T> list) {
+        return new PageResponse<>(list, PageInfo.ofList(list.size()));
+    }
 }
