3e16da43084596479aef2afd5fcb67ee1dbe8e8e
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-05-21 18:54:13 +0900

    feat: CloseReason 상태값 추가

---
+++ src/main/java/matchuri/backend/domain/recommendation/entity/PersonalRecommendationCloseReason.java
@@ -0,0 +1,8 @@
+package matchuri.backend.domain.recommendation.entity;
+
+public enum PersonalRecommendationCloseReason {
+    SELECTED,
+    REROLLED_WITH_SKIP,
+    REROLLED_WITHOUT_SKIP,
+    EXPIRED
+}
