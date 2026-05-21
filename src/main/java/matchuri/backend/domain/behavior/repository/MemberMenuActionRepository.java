82bf8fa46ee16d67e57c62b2fe85153dd3f553b0
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-05-21 20:37:54 +0900

    feat: 특정 기간 내 특정 action type의 사용자 행동 리스트 쿼리

--- src/main/java/matchuri/backend/domain/behavior/repository/MemberMenuActionRepository.java
+++ src/main/java/matchuri/backend/domain/behavior/repository/MemberMenuActionRepository.java
@@ -1,9 +1,17 @@
 package matchuri.backend.domain.behavior.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import matchuri.backend.domain.behavior.entity.ActionType;
 import matchuri.backend.domain.behavior.entity.MemberMenuAction;
 import org.jspecify.annotations.NullMarked;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @NullMarked
 public interface MemberMenuActionRepository extends JpaRepository<MemberMenuAction, Long> {
+    List<MemberMenuAction> findByMemberIdAndActionTypeAndCreatedAtAfter(
+            Long memberId,
+            ActionType actionType,
+            LocalDateTime createdAt
+    );
 }
